9ba058093eef068d06c5f01e49662958d13022e6
Farrelhasyidan <farrelhasyidan@gmail.com>
2021-01-29 11:25:09 +0700

    first commit

--- Navigation.UI/ContentView.swift
+++ Navigation.UI/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View{
+        NavigationView{
+            Text("Hello ini content")
+            
+                .navigationBarItems(
+                
+                leading:
+                    HStack{
+                        Button(action: {print("Hello Button")}){
+                            Image("youtube_logo")
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 90, height: 20)
+                        }
+                    },
+                
+                trailing:
+                    HStack(spacing:20){
+                        Button(action: {print("Hello Button")}){
+                            Image(systemName: "tray")
+                                .foregroundColor(.black)
+                        }
+                        
+                        Button(action: {print("Hello Button")}){
+                            Image(systemName: "video.fill")
+                                .foregroundColor(.black)
+                        }
+                        
+                        Button(action: {print("Hello Button")}){
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.black)
+                        }
+                        
+                        Button(action: {print("Hello Button")}){
+                            Image("profile")
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+            )
+            
+        }
     }
 }
